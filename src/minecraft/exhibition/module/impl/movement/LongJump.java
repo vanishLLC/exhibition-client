aeb6a1fbed14432b9442a240fb006003edd7ef72
Ben <cool101dood@gmail.com>
2021-01-01 17:21:02 -0800

    LongJump standing silent flag fix?

--- src/minecraft/exhibition/module/impl/movement/LongJump.java
+++ src/minecraft/exhibition/module/impl/movement/LongJump.java
@@ -420,26 +420,24 @@ public class LongJump extends Module {
                             bruhTick++;
 
                             // Movement fix to make the movement > 0.00090D
-/*                            double var3 = mc.thePlayer.posX - mc.thePlayer.lastReportedPosX;
+                            double var3 = mc.thePlayer.posX - mc.thePlayer.lastReportedPosX;
                             double var5 = (em.getY() + bruh) - mc.thePlayer.lastReportedPosY;
                             double var7 = mc.thePlayer.posZ - mc.thePlayer.lastReportedPosZ;
 
                             if (var3 * var3 + var5 * var5 + var7 * var7 <= 0.00090D) {
                                 if (tickPassed == 1) {
-                                    up = true;
-                                    bruh = 0.000024F + (float) (min + (max - min) * Math.random()); // 0.00079
+                                    bruh = 0.000924F + (float) (min + (max - min) * Math.random()); // 0.00079
                                 }
                                 if (tickPassed == 2) {
-                                    bruh = 0.000014F + (float) (min + (max - min) * Math.random()); // -0.00031
+                                    bruh = 0.000914F + (float) (min + (max - min) * Math.random()); // -0.00031
                                 }
                                 if (tickPassed == 3) {
-                                    up = true;
-                                    bruh = 0.000024F + (float) (min + (max - min) * Math.random()); // -0.00069
+                                    bruh = 0.000924F + (float) (min + (max - min) * Math.random()); // -0.00069
                                 }
                                 if (tickPassed == 4) {
-                                    bruh = 0.000017F + (float) (min + (max - min) * Math.random()); // 0.0012
+                                    bruh = 0.000917F + (float) (min + (max - min) * Math.random()); // 0.0012
                                 }
-                            }*/
+                            }
 
                             if ((boolean) settings.get(C13PACKET).getValue() && bruhTick > 0 && (bruhTick % (20 * 6) == 0)) {
                                 sendC13Packet();
