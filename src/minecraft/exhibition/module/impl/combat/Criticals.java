dddf833b3b1199033057978a415615fa2ef74eef
Neohacks <officialneohack@gmail.com>
2021-01-16 07:21:06 -0600

    fixed watchdog ban in duels

--- src/minecraft/exhibition/module/impl/combat/Criticals.java
+++ src/minecraft/exhibition/module/impl/combat/Criticals.java
@@ -62,9 +62,10 @@ public class Criticals extends Module {
     }
 
     private boolean spoof() {
-        Bypass bypass = Client.getModuleManager().getCast(Bypass.class);
+/*        Bypass bypass = Client.getModuleManager().getCast(Bypass.class);
         boolean allowInvalidCrits = bypass.allowBypassing() && (bypass.bruh > 10 && bypass.bruh % 100 > 10 && bypass.bruh % 100 < 99) && HypixelUtil.isVerifiedHypixel();
-        return allowInvalidCrits && !silentCrits && ((Options) settings.get(PACKET).getValue()).getSelected().startsWith("Packet") && (HypixelUtil.isInGame("DUEL") || HypixelUtil.isInGame("HYPIXEL PIT"));
+        return allowInvalidCrits && !silentCrits && ((Options) settings.get(PACKET).getValue()).getSelected().startsWith("Packet") && (HypixelUtil.isInGame("DUEL") || HypixelUtil.isInGame("HYPIXEL PIT"));*/
+        return HypixelUtil.isInGame("HYPIXEL PIT");
     }
 
     public boolean isPacket() {
